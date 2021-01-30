c491b21a97f53c6e3c0c8d31a0513d28b79c035b
Ifanj <ifan.johnston@gmail.com>
2021-01-30 13:30:24 +0100

    added server files

--- server/scripts/server_setup.sh
+++ server/scripts/server_setup.sh
@@ -6,6 +6,9 @@ apt upgrade -y
 apt install zsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
+# Install some OS dependencies:
+sudo apt-get install -y -q build-essential git zip unload tree
+sudo apt-get install -y -q python3-pip python3-dev python3-venv
 
 # Stop the hackers
 sudo apt install fail2ban -y
@@ -38,4 +41,24 @@ pip install --upgrade gunicorn uvloop httptools
 
 # clone the repo:
 cd /apps
-git clone https://github.com/
+git clone https://github.com/idjotherwise/snow-watcher app_repo
+
+# setup the webapp
+cp /apps/app_repo/server/units/weather.service /etc/systemd/system/
+
+systemctl start weather
+systemctl status weather
+systemctl enable weather
+
+# setup public facing server
+apt install nginx
+
+# rm /etc/nginx/sites-enabled/default
+
+cp /apps/app_repo/server/nginx/weather.nginx /etc/nginx/sites-enabled
+update-rc.d nginx enable
+service nginx restart
+
+add-apt-repository ppa:certbot/certbot
+apt install python3-certbox-nginx
+certbot --nginx -d weatherotherwise.xyz
